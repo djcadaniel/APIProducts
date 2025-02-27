58caa41e1b7a5c5e70d000c94315257b615ee0f2
djcadaniel <djcadaniel@gmail.com>
2025-02-27 16:53:31 -0500

    delet dialectoptions

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,7 +10,7 @@ const db = new Sequelize(process.env.DATABASE_URL!,{
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      // rejectUnauthorized: false
     }
   }
   // logging: false
